aee5244de536535b7c4734f62323d27a9c7dc9ff
May Thazin Oo <maythazinoo1234d@gmail.com>
2024-05-09 09:24:07 +0630

    sample crud commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Article\ArticleController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/articles', function () {
+// return 'Article List';
+// });
+// Route::get('/articles/detail', function () {
+// return 'Article Detail';
+// })->name('article.detail');
+// Route::get('/articles/detail/{id}', function ( $id ) {
+// return "Article Detail - $id";
+// });
+
+
+// Route::get('/articles/more', function() {
+// return redirect()->route('article.detail');
+// });
+
+Route::get('/', [ArticleController::class, 'index']);
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [
+ArticleController::class,
+'detail'
+]);
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::get('/articles/add', [ArticleController::class, 'add']);
+Route::post('/articles/add', [
+ArticleController::class,
+'create'
+]);
+Route::get('/articles/delete/{id}', [
+ArticleController::class,
+'delete'
+]);
